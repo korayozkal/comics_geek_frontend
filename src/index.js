22c861e1037d4118cb9073734649ca4cff619e60
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-10-20 22:37:09 -0700

    index working on frontend

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
+const endPoint = "http://localhost:3000/comicbooks"
+
 document.addEventListener("DOMContentLoaded", () => {
-    alert("LOADED");
-    //console.log("LOADED");
-});
+    getComics()
+})
+
+function getComics() {
+    fetch(endPoint)
+        .then(response => response.json())
+        .then(comics => {
+            comics.data.forEach(comicbook => {
+                const comicbooksMarkup = `
+          <div data-id=${comicbook.id}>
+            <img src=${comicbook.attributes.image_url} height="300" width="200">
+            <h3>${comicbook.attributes.title}</h3>
+            <p>${comicbook.attributes.publisher.name}</p>
+            <button data-id=${comicbook.id}>edit</button>
+          </div>
+          <br><br>`;
+
+                document.querySelector('#comicbooks-container').innerHTML += comicbooksMarkup;
+            })
+        })
+}
